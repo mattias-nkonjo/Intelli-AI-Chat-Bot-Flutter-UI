e51e6eca7cc40a003cdba2100ca511817d464f26
MATTIAS NKONJO <nkonjomattias@gmail.com>
2023-02-07 14:12:27 +0200

    1. Created Flutter Project 2. Used com.flutterai as a Domain Name 3. Added flutter_riverpod: ^2.1.3 4. Added go_router: ^6.0.4 5. Added firebase_auth: ^4.2.5 6. Added firebase_core: ^2.4.1 7. Create Web , iOS and Android App in Firebase 8. Connected the App with Firebase

---
+++ android/app/src/main/kotlin/com/flutterai/p1_flutter_ai/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterai.p1_flutter_ai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:p1_flutter_ai/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Flutter before anything else
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'p1 Flutter AI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(' Hello I am tp1 Flutter AI'),
+      ),
+    );
+  }
+}
